4a485a7f33b5abca27a2a9197816e5b28ac709aa
Rishiraj <53556561+wabrishi@users.noreply.github.com>
2022-12-27 15:56:41 +0000

    2D array

---
+++ C++/scanAndPrint2Darray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main()
+{
+    int arr[2][3];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cin>>arr[i][j];
+            /* code */
+        }
+        
+        /* code */
+    }
+        for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout<<arr[i][j];
+            /* code */
+        }
+        
+        /* code */
+    }
+    return 0;
+
+}

c94b6c1a76d44eada59f21f1167133fde0d3c669
Rishiraj <53556561+wabrishi@users.noreply.github.com>
2022-12-29 07:28:45 +0000

    Area Of Rectangle

---
+++ C/Unit_1/areaOfRectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Note: - The Program Provide Results in Same Unit as You Provide length and Breath\n");
+    printf("Enter The Length And Breath Of Rectangle= ");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("Area Of The Given Rectangle is = %d\n", c);
+    return 0;
+}

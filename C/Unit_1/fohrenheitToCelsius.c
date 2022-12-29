0965c54e1bea40ed48f77784186604dfedda9ee0
Rishiraj <53556561+wabrishi@users.noreply.github.com>
2022-12-29 07:20:24 +0000

    SUm of Number

---
+++ C/Unit_1/fohrenheitToCelsius.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    char c;
+    printf("F - for Celsius to Fahrenheit\nC - for Fahrenheit To Celsius\n");
+    printf("Enter the Charecter(F or C) = ");
+    scanf("%s",&c);
+    // printf("Enter the number to covert");
+    // scanf("%f",&a);
+    switch (c)
+    {
+    case 'C':
+    case 'c':
+        printf("Enter the number in Fahrenheit to convert in Celsius = ");
+        scanf("%f",&a);
+        b=(a-32)*5/9;
+        printf("%f\n", b);
+        break;
+    case 'f':
+    case 'F':
+        printf("Enter the number in Celsius to convert in Fahrenheit = ");
+        scanf("%f",&a);
+        b=(a*9/5)+32;
+        printf("%f\n", b);
+        break;
+    
+    
+    default:
+        break;
+    }
+}
---
+++ C/Unit_1/sumOfTwoNumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two number to add them= ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of Given number is = %d\n", c);
+    return 0;
+}

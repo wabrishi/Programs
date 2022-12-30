1683bfecf0e518fd4f5c9f511e32fd490069ce22
Rishiraj <53556561+wabrishi@users.noreply.github.com>
2022-12-30 17:21:26 +0000

    Root of equtation

--- C/Unit_1/rootsOfQuadraticEquation.c
+++ C/Unit_1/rootsOfQuadraticEquation.c
@@ -3,10 +3,29 @@
 
 int main()
 {
-   int b,c=2;
-   int d =10;
+   int a,b,c,d;
    float x1,x2;
-   float a= pow(d,c); 
-	printf("\n%f\n",a);
+ 
+   printf("Input the value of a,b & c : ");
+   scanf("%d%d%d",&a,&b,&c);
+   d=b*b-4*a*c;
+   if(d==0)
+   {
+     printf("Both roots are equal.\n");
+     x1=-b/(2.0*a);
+     x2=x1;
+     printf("First  Root Root1= %f\n",x1);
+     printf("Second Root Root2= %f\n",x2);
+   }
+   else if(d>0)
+	{
+	   printf("Both roots are real and diff-2\n");
+	   x1=(-b+sqrt(d))/(2*a);
+	   x2=(-b-sqrt(d))/(2*a);
+	   printf("First  Root Root1= %f\n",x1);
+	   printf("Second Root root2= %f\n",x2);
+	}
+	else
+	    printf("Root are imeainary;\nNo Solution. \n");
 	return 0;
 }
